721a680fc10862c7a56d19e876519685c916b029
tiejean <taijingchen99@gmail.com>
2025-05-19 10:02:30 -0500

    update debug_data_collection

--- virtualhome/demo/debug_examples/debug_data_collection.py
+++ virtualhome/demo/debug_examples/debug_data_collection.py
@@ -9,6 +9,9 @@ sys.path.append(".")
 from utils_demo import *
 from graph_utils import *
 
+ambiguous_manipulable_objects = ["book", "dishbowl", "pillow", "clothespile", "clothesshirt", "clothespant", "towel", "folder"]
+
+
 def example_expand_scene(comm):
     comm.reset(4)
     success, graph = comm.environment_graph()
@@ -98,10 +101,73 @@ def example_generate_walk_find_script(comm):
     success, message = comm.expand_scene(graph)
     print("[Case 2.3]", message) # {}
     
+def example_single_object_replacement(comm):
+    input_path = os.path.abspath('../../unity_output/')
+    output_path = os.path.abspath('../../outputs/')
+    relationships = load_relationships("config/relationships.txt")
+    prefix = "test"
+    image_dir = os.path.join('../../unity_output/', prefix)
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    for root, _, files in os.walk(image_dir):
+        for file in files:
+            filepath = os.path.join(root, file)
+            os.remove(filepath)
+    
+    comm.reset(4)
+    random.seed(40)
+    
+    success, graph_before = comm.environment_graph()
+    success, graph = comm.environment_graph()
+    comm.add_character('chars/Female2', initial_room='bathroom')
+    _, ambiguous_manipulable_nodes, _ = find_nodes_and_edges_by_class(graph, ambiguous_manipulable_objects, verbose=False)
+    nodes = filter_nodes_by_class(ambiguous_manipulable_nodes, ["book"])
+    
+    for node in nodes:
+        script = generate_single_object_replacement_script(graph, node, relationships, verbose=False)
+        success, message = comm.render_script(script=script,
+                                processing_time_limit=1000,
+                                find_solution=False,
+                                image_width=640,
+                                image_height=480,  
+                                skip_animation=True, # False,
+                                recording=False, # True,
+                                save_pose_data=True,
+                                file_name_prefix=prefix)
+        print("render_script success: ", success)
+        
+    success, graph = comm.environment_graph()
+    script = generate_walk_find_script(graph, ["book"])
+    print(script)
+    success, message = comm.render_script(script=script,
+                                        processing_time_limit=1000,
+                                        find_solution=False,
+                                        image_width=640,
+                                        image_height=480,  
+                                        skip_animation=False,
+                                        recording=True,
+                                        save_pose_data=True,
+                                        camera_mode=["FIRST_PERSON"],
+                                        file_name_prefix=prefix)
+    print("render_script success: ", message)
+    
+    success, graph = comm.environment_graph()
+    graph = remove_nodes_by_classes(graph, ["character"])
+    success, message = comm.expand_scene(graph)
+    print("[Case 3.1]", message) #  {}
+    if success:
+        utils_viz.generate_video(input_path=input_path, prefix="test", output_path=output_path)
+
+    success, graph_after = comm.environment_graph()
+    diff_node_edges(graph_before, graph_after)
+    
+    import pdb; pdb.set_trace()
+    print()
 
 if __name__ == "__main__":
     comm = UnityCommunication(port="8080")
     comm.timeout_wait = 300
     
-    example_expand_scene(comm)
-    example_generate_walk_find_script(comm)
+    # example_expand_scene(comm)
+    # example_generate_walk_find_script(comm)
+    example(comm)
